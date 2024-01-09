b0b4bf37b07002da68367962bf5beca19622e2f7
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2024-01-09 11:45:59 +0100

    Add functionality to add new flashcards

--- src/pages/PageManageFlashcards.tsx
+++ src/pages/PageManageFlashcards.tsx
@@ -1,40 +1,82 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../AppContext";
-import { MdModeEditOutline } from "react-icons/md";
+import { MdModeEditOutline, MdCancel } from "react-icons/md";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { SiOneplus } from "react-icons/si";
+import { FaSave } from "react-icons/fa";
+import { INewFlashcard, blankNewFlashcard } from "../shared/interfaces";
 
 export const PageManageFlashcards = () => {
 	const { flashcards } = useContext(AppContext);
+	const [isAddingFlashcard, setIsAddingFlashcard] = useState(false);
+	const [newFlashcard, setNewFlashcard] =
+		useState<INewFlashcard>(blankNewFlashcard);
 	return (
 		<>
-		<p>There are {flashcards.length} flashcards:</p>
+			<p>There are {flashcards.length} flashcards:</p>
 
-		<table className="dataTable mt-4">
-				<thead>
-					<tr>
-						<th>SUUID</th>
-						<th>Category</th>
-						<th>Front</th>
-						<th>Back</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					{flashcards.map(flashcard => {
-						return (
-							<tr key={flashcard.suuid}>
-								<td>{flashcard.suuid}</td>
-								<td>{flashcard.category}</td>
-								<td>{flashcard.front}</td>
-								<td>{flashcard.back}</td>
+			<form>
+				<table className="dataTable mt-4 w-[60rem]">
+					<thead>
+						<tr>
+							<th>SUUID</th>
+							<th>Category</th>
+							<th>Front</th>
+							<th>Back</th>
+							<th>
+								<div className="flex justify-center text-[#222] text-2xl">
+									<SiOneplus
+										onClick={() =>
+											setIsAddingFlashcard(
+												!isAddingFlashcard
+											)
+										}
+										className="cursor-pointer hover:text-green-900"
+									/>
+								</div>
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{isAddingFlashcard && (
+							<tr>
+								<td></td>
 								<td>
-								<div className="flex"></div>	
-									<MdModeEditOutline /><RiDeleteBin6Line /></td>
+									<input className="w-full" />
+								</td>
+								<td>
+									<input className="w-full" />
+								</td>
+								<td>
+									<input className="w-full" />
+								</td>
+								<td>
+									<div className="flex gap-1">
+										<FaSave className="cursor-pointer hover:text-green-900" />
+										<MdCancel className="cursor-pointer hover:text-red-900" />
+									</div>
+								</td>
 							</tr>
-						)
-					})}
-				</tbody>
-		</table>
+						)}
+						{flashcards.map((flashcard) => {
+							return (
+								<tr key={flashcard.suuid}>
+									<td>{flashcard.suuid}</td>
+									<td>{flashcard.category}</td>
+									<td>{flashcard.front}</td>
+									<td>{flashcard.back}</td>
+									<td>
+										<div className="flex gap-1">
+											<MdModeEditOutline className="cursor-pointer hover:text-green-900" />
+											<RiDeleteBin6Line className="cursor-pointer hover:text-red-900" />
+										</div>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</form>
 		</>
-	)
-}
+	);
+};
